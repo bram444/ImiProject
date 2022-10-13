3854b81d69e57874e61a2c4fe8c603c6c78b6145
Brissinck Bram <72300838+bram444@users.noreply.github.com>
2022-10-13 10:02:22 +0200

    Add Interface Repositories

---
+++ src/Imi.Project.Api.Core/Repository/Interfaces/IGameGenreRepository.cs
@@ -0,0 +1,21 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Repository.Interfaces
+{
+    public interface IGameGenreRepository
+    {
+        IQueryable<GameGenre> GetAll();
+        Task<IEnumerable<GameGenre>> ListAllAsync();
+        Task<GameGenre> GetByGameIdAsync(Guid id);
+        Task<GameGenre> GetByGenreIdAsync(Guid id);
+
+        Task<GameGenre> UpdateAsync(GameGenre entity);
+        Task<GameGenre> AddAsync(GameGenre entity);
+        Task<GameGenre> DeleteAsync(GameGenre entity);
+    }
+}
---
+++ src/Imi.Project.Api.Core/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Repository.Interfaces
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<IEnumerable<User>> SearchUserNameAsync(string search);
+        Task<IEnumerable<User>> SearchFirstNameAsync(string search);
+        Task<IEnumerable<User>> SearchLastNameAsync(string search);
+
+    }
+}
