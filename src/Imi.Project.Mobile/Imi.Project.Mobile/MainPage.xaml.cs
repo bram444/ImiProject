725869be2925e9a35486113115cd017c451795e1
Brissinck Bram <72300838+bram444@users.noreply.github.com>
2022-10-27 09:13:29 +0200

    Add pages

--- src/Imi.Project.Mobile/Imi.Project.Mobile/MainPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/MainPage.xaml.cs
@@ -14,5 +14,25 @@ namespace Imi.Project.Mobile
         {
             InitializeComponent();
         }
+
+        private async void btnGame_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage(), true);
+        }
+
+        private async void btnGenre_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GenrePage(), true);
+        }
+
+        private async void btnPublisher_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PublisherPage(), true);
+        }
+
+        private async void btnUser_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new UserPage(), true);
+        }
     }
 }
