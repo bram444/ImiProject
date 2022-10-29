95c6fb4e871b832e84e8fe4de9acd8be991733f5
Brissinck Bram <72300838+bram444@users.noreply.github.com>
2022-10-29 15:42:53 +0200

    Add validators

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Validators/GameInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Domain.Validators
+{
+    public class GameInfoValidator : AbstractValidator<GamesInfo>
+    {
+        public GameInfoValidator()
+        {
+            RuleFor(gameinfo => gameinfo.Name)
+                .NotEmpty()
+                .WithMessage("Game name cannot be empty");
+        }
+    }
+}
